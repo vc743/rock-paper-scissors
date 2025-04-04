798d33b9cefa9f3cc6296bae021f9b716347a4fd
vc743 <victorcontrerasvargas55@gmail.com>
2025-04-03 23:25:18 -0400

    Computer choice added

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  switch (randomNumber) {
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    case 3:
+      return "scissors";
+  }
+}
